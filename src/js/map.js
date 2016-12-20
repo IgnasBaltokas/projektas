0bb26825aa94913724899c11efd3964852768ba0
Deividas123 <deividukkas@gmail.com>
2016-12-20 20:30:50 +0200

    lyg virsus ir sutvarkytas

---
+++ src/js/map.js
@@ -0,0 +1,12 @@
+/**
+ * Created by HP on 2016.12.20.
+ */
+<script>
+function myMap() {
+    var mapOptions = {
+        center: new google.maps.LatLng(51.5, -0.12),
+        zoom: 10,
+        mapTypeId: google.maps.MapTypeId.HYBRID
+    }
+    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+}
